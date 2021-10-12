a5da37489d46064ec9a6373dbdbfb1c5ced239da
Riyara Master <rtmaster@udel.edu>
2021-10-12 14:04:04 -0400

    Added my own name - Riyara

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ function App(): JSX.Element {
                     Edit <code>src/App.tsx</code> and save to reload.
                 </p>
                 <p>Dara McNally</p>
+                <p>Riyara Master</p>
             </header>
         </div>
     );
