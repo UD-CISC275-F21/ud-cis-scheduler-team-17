14d4e72f65b08660b23958c6f3d9bd90044f0f7a
DM <mcnally@udel.edu>
2021-10-20 13:53:30 -0400

    github was still mad

--- src/components/AddTableModal.tsx
+++ src/components/AddTableModal.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
-import { Button, Form, Modal} from "react-bootstrap";
-import { Subject } from "../interfaces/subject";  // I renamed table interface to subject
+//import { useState } from "react";
+//import { Button, Form, Modal} from "react-bootstrap";
+//import { Subject } from "../interfaces/subject";  // I renamed table interface to subject
 
