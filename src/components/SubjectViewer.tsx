3db0f8694d3c900dfa8634f718481472ddcc54e0
DM <mcnally@udel.edu>
2021-10-20 13:41:59 -0400

    Minor edits to display and parameters

--- src/components/SubjectViewer.tsx
+++ src/components/SubjectViewer.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import {Subject} from "../interfaces/subject";
+import { Card, Row } from "react-bootstrap";
+import { TableFace } from "../interfaces/tableface";
 
-export function SubjectTable() : JSX.Element {
+export function SubjectTable({currentSem}:{
+    currentSem: number
+}) : JSX.Element {
     const subjectList: Subject[] = [
         {id: "CISC106", name: "General Computer Science for Engineers", credits: 3},
         {id: "CISC", name: "ClassName", credits: 3},
@@ -10,16 +14,19 @@ export function SubjectTable() : JSX.Element {
         {id: "CISC", name: "ClassName", credits: 3}];
     
     return (
-        <table>
-            <tr><th>Class ID</th><th>Class Name</th><th>Credits</th></tr>
-            { subjectList.map((sbj: Subject) => {
-                return (<tr key={sbj.name}>
-                    <td>{sbj.id}</td>
-                    <td>{sbj.name}</td>
-                    <td>{sbj.credits}</td>
-                </tr>);
-            })}
-        </table>
+        <Card>
+            <Row><strong>Semester {currentSem}</strong></Row>
+            <table>
+                <tr><th>Class ID</th><th>Class Name</th><th>Credits</th></tr>
+                { subjectList.map((sbj: Subject) => {
+                    return (<tr key={sbj.name}>
+                        <td>{sbj.id}</td>
+                        <td>{sbj.name}</td>
+                        <td>{sbj.credits}</td>
+                    </tr>);
+                })}
+            </table>
+        </Card>
     );
     //Table setup credit to Dr. Bart
 }
