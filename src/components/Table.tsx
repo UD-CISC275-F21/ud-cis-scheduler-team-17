52d3b86ddc7194af1ce7b400d41113fe67e46db1
DM <mcnally@udel.edu>
2021-10-28 10:10:16 -0400

    Deleted some unnecessary comments

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -8,7 +8,6 @@ import { useState } from "react";
 I think for this it should return a card and several subject components (which will need to be made for ease of adding classes).
 It should have the buttons to add courses.
 3 columns 5 rows default?
--NEEDS SUBJECTVIEWER COMPONENT FOR RETURN
 -NEEDS "ADD CLASS" BUTTON
 */
 export function OurTable() : JSX.Element {
